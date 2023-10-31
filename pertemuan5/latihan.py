28bddedf341916deab23cd6acff5fb1f32b04602
Ibra2319ti <146930756+Ibra2319ti@users.noreply.github.com>
2023-10-31 18:30:11 +0700

    Add files via upload

---
+++ pertemuan5/latihan.py
@@ -0,0 +1,13 @@
+a = {1,2,3,4,5}
+b = {"12345","daud",11,12,13}
+print (a)
+print (b)
+
+cuaca = input ("apakah cuaca hari ini?")
+match cuaca:
+    case "hujan":
+        print("berangkat kuliah memakai jas hujan")
+    case "badai":
+        print("tidak berangkat kulaih")
+    case "panas" :
+        print("berangkat kuliah menggunakan mobil") 
---
+++ pertemuan5/pertemuan5.py
@@ -0,0 +1,27 @@
+
+kendaraan = ["24688","beat",125,"hitam"]
+kendaraan.append("8.000.000")
+kendaraan.append("2")
+kendaraan.insert(2, "matic")
+kendaraan.insert (1,"beat")
+print (kendaraan) 
+
+pilihan = int(input("masukan pilihan : "))
+match pilihan:
+    case 1:
+        luas_persegi = int(input("masukan nilai sisi : "))
+        hasilLuas = luas_persegi * luas_persegi
+        print("luas persegi adalah",hasilLuas)
+    case 2:
+        nilai_phi = float(input("masukan nilai phi :"))
+        r = int(input("masukan nilai Jari jari :"))
+        jari_jari  = nilai_Jari * nilai_Jari
+        luaslingkaran = 3.14 * r*r
+        print("luas lingkaran adalah:",luaslingkaran)
+    case 3:
+        
+         alas = int(input(* alas +*))
+         tinggi = in(input(*tinggi = *))
+        
+        
+        
